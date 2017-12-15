3059d48222982ae9e56835dcae9e0f77b499ffcf
Alex Whitney <adwhit@fastmail.com>
2017-12-15 19:12:12 +0000

    day7 v2

--- src/bin/07_recursive_circus.rs
+++ src/bin/07_recursive_circus.rs
@@ -1,10 +1,13 @@
 extern crate advent_of_code;
 extern crate failure;
 extern crate regex;
+extern crate itertools;
 
 use regex::Regex;
 
 use advent_of_code::Result;
+
+use itertools::Either;
 
 use std::fs;
 use std::io::prelude::*;
@@ -12,8 +15,8 @@ use std::collections::HashMap;
 
 type Map = HashMap<String, (u32, Option<String>, Vec<String>)>;
 
-fn get_data() -> Result<Map> {
-    let mut f = fs::File::open("data/07.txt")?;
+fn get_data(path: &str) -> Result<Map> {
+    let mut f = fs::File::open(path)?;
     let mut s = String::new();
     f.read_to_string(&mut s)?;
     let mut map = HashMap::new();
@@ -43,9 +46,7 @@ fn add_parents(map: &mut Map) {
         }
     }
 }
-
-fn circus_v1(map: &mut Map) -> String {
-    add_parents(map);
+fn get_root(map: &Map) -> String {
     let mut name = map.keys().nth(1).unwrap().to_string();
     loop {
         let (_, ref parent, _) = map[&name];
@@ -57,13 +58,63 @@ fn circus_v1(map: &mut Map) -> String {
     }
 }
 
+fn circus_v1(map: &mut Map) -> String {
+    add_parents(map);
+    get_root(map)
+}
+
+fn get_node_weight(node: &str, map: &Map) -> Either<(u32, u32), u32> {
+    let (ref weight, _, ref children) = map[node];
+    let mut child_weights = Vec::new();
+    for c in children {
+        match get_node_weight(c, map) {
+            Either::Left((sum, w)) => child_weights.push((sum, w)),
+            Either::Right(w) => return Either::Right(w)
+        }
+    }
+    if child_weights.len() > 2 {
+        // check they are even
+        let mut map = HashMap::new();
+        for &(s, w) in &child_weights {
+            let v = map.entry(s + w).or_insert(0);
+            *v += 1
+        }
+        if map.len() > 1 {
+            let mut kvs: Vec<_> = map.iter().collect();
+            kvs.sort_by_key(|&(_, v)| v);
+            let good_weight = *kvs[1].0;
+            let bad_weight = *kvs[0].0;
+            let diff = good_weight as i32 - bad_weight as i32;
+            for (s, w) in child_weights {
+                if s + w == bad_weight {
+                    let res = w as i32 + diff;
+                    return Either::Right(res as u32);
+                }
+            }
+            unreachable!()
+        }
+    }
+    let sum: u32 = child_weights.iter().map(|&(s, w)| s + w).sum();
+    Either::Left((sum, *weight))
+}
+
+fn circus_v2(map: &mut Map) -> u32 {
+    add_parents(map);
+    let root = get_root(map);
+    match get_node_weight(&root, map) {
+        Either::Left(_) => panic!("Failed to find weight"),
+        Either::Right(v) => v
+    }
+}
+
+
 fn run() -> Result<()> {
-    let mut data = get_data()?;
+    let mut data = get_data("data/07.txt")?;
     let outcome1 = circus_v1(&mut data);
     println!("v1: {}", outcome1);
-    // let mut data = get_data()?;
-    // let outcome2 = reallocate_v2(&mut data);
-    // println!("v2: {}", outcome2);
+    let mut data = get_data("data/07.txt")?;
+    let outcome2 = circus_v2(&mut data);
+    println!("v2: {}", outcome2);
     Ok(())
 }
 
@@ -79,13 +130,10 @@ fn main() {
 mod tests {
     use super::*;
 
-    // #[test]
-    // fn cases_v1() {
-    //     assert_eq!(reallocate_v1(&mut [0, 2, 7, 0]), 5)
-    // }
-
-    // #[test]
-    // fn cases_v2() {
-    //     assert_eq!(reallocate_v2(&mut [0, 2, 7, 0]), 4)
-    // }
+    #[test]
+    fn cases_v2() {
+        let mut data = get_data("data/07_test.txt").unwrap();
+        let outcome2 = circus_v2(&mut data);
+        assert_eq!(outcome2, 243)
+    }
 }
