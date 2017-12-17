2caa3341836196dd005e930bf6c0834544cef75c
Alex Whitney <adwhit@fastmail.com>
2017-12-17 12:36:57 +0000

    day 9 v2

--- src/bin/09_stream_processing.rs
+++ src/bin/09_stream_processing.rs
@@ -23,23 +23,24 @@ enum State {
     End,
 }
 
-fn process_stream(stream: &[u8]) -> Result<i32> {
+fn process_stream(stream: &[u8]) -> Result<(i32, u32)> {
     use State::*;
     let mut state = Start;
     let mut ct = 0;
+    let mut garbagect = 0;
     let mut depth = 0;
     let mut iter = stream.iter();
     while let Some(s) = iter.next() {
         state = match *s {
             b'{' => match state {
                 Start | InGroup => { depth += 1; InGroup},
-                InGarbage => InGarbage,
                 End => bail!("Bad: {}", *s as char),
+                InGarbage => {garbagect += 1; InGarbage},
             },
             b'<' => match state {
                 End => bail!("Bad: {}", *s as char),
                 Start | InGroup => InGarbage,
-                InGarbage => InGarbage
+                InGarbage => {garbagect += 1; InGarbage},
             },
             b'>' => match state {
                 Start | End | InGroup => bail!("Bad: {}", *s as char),
@@ -48,7 +49,7 @@ fn process_stream(stream: &[u8]) -> Result<i32> {
             b'}' => match state {
                 Start => bail!("Bad: {}", *s as char),
                 InGroup | End => { ct += depth; depth -= 1; End },
-                InGarbage => InGarbage,
+                InGarbage => {garbagect += 1; InGarbage},
             },
             b'!' => match state {
                 Start | End | InGroup => bail!("Bad: {}", *s as char),
@@ -57,23 +58,25 @@ fn process_stream(stream: &[u8]) -> Result<i32> {
             b',' => match state {
                 Start => bail!("Bad: {}", *s as char),
                 End => Start,
-                InGarbage | InGroup => state,
+                InGroup => InGroup,
+                InGarbage => {garbagect += 1; InGarbage},
             },
             b'\n' => state,
             _ => match state {
                 Start | InGroup | End => bail!("Bad: {}", *s as char),
-                InGarbage => InGarbage,
+                InGarbage => {garbagect += 1; InGarbage},
             },
         };
     }
     assert_eq!(depth, 0);
-    Ok(ct)
+    Ok((ct, garbagect))
 }
 
 fn run() -> Result<()> {
     let data = get_data("data/09.txt")?;
     let outcome1 = process_stream(&data)?;
-    println!("v1: {}", outcome1);
+    println!("v1: {}", outcome1.0);
+    println!("v2: {}", outcome1.1);
     // let outcome2 = circus_v2(&mut data);
     // println!("v2: {}", outcome2);
     Ok(())
@@ -96,10 +99,10 @@ mod tests {
     fn cases_v1() {
         let data = b"{{<a!>},{<a!>},{<a!>},{<ab>}}";
         let outcome = process_stream(data).unwrap();
-        assert_eq!(outcome, 3);
+        assert_eq!(outcome, (3, 17));
 
         let data = b"{{<!!>},{<!!>},{<!!>},{<!!>}}";
         let outcome = process_stream(data).unwrap();
-        assert_eq!(outcome, 9);
+        assert_eq!(outcome, (9, 0));
     }
 }
