82a42363d54891663d70770c98787f22df2ec220
Alex Whitney <adwhit@fastmail.com>
2017-12-17 22:51:56 +0000

    day10 v2

--- src/bin/10_knot_hash.rs
+++ src/bin/10_knot_hash.rs
@@ -20,6 +20,15 @@ fn get_data(path: &str) -> Result<Vec<u8>> {
         ).collect()
 }
 
+fn get_data2(path: &str) -> Result<Vec<u8>> {
+    let mut f = fs::File::open(path)?;
+    let mut s = Vec::new();
+    f.read_to_end(&mut s)?;
+    s.pop(); // remove newline
+    s.extend(&[17, 31, 73, 47, 23]);
+    Ok(s)
+}
+
 #[derive(Clone, Debug)]
 struct State {
     data: Vec<u8>,
@@ -44,6 +53,23 @@ impl State {
         self.curpos = (self.curpos + len + self.skip) % l;
         self.skip = (self.skip + 1) % l;
     }
+
+    fn dense_hash(&self) -> String {
+        let mut out = String::new();
+        for round in 0..16 {
+            let mut v = self.data[round * 16];
+            for roundix in 1..16 {
+                let ix = round * 16 + roundix;
+                v ^= self.data[ix]
+            }
+            if v < 16 {
+                out.push_str("0")
+            }
+            out.push_str(&format!("{:x}", v))
+        }
+        assert!(out.len() == 32);
+        out
+    }
 }
 
 fn knot_hash(state: &mut State, lens: &[u8]) -> u32 {
@@ -53,14 +79,29 @@ fn knot_hash(state: &mut State, lens: &[u8]) -> u32 {
     state.data[0] as u32 * state.data[1] as u32
 }
 
+fn knot_hash2(state: &mut State, lens: &[u8]) -> String {
+    for _ in 0..64 {
+        for len in lens {
+            state.hash(*len as usize)
+        }
+    }
+    state.dense_hash()
+}
+
 fn run() -> Result<()> {
-    let data = get_data("data/10.txt")?;
-    let mut state = State::new(255);
-    let outcome = knot_hash(&mut state, &data);
-    println!("v1: {}", outcome);
-    // println!("v2: {}", outcome1.1);
-    // let outcome2 = circus_v2(&mut data);
-    // println!("v2: {}", outcome2);
+    {
+        let data = get_data("data/10.txt")?;
+        let mut state = State::new(255);
+        let outcome = knot_hash(&mut state, &data);
+        println!("v1: {}", outcome);
+    }
+
+    {
+        let data = get_data2("data/10.txt")?;
+        let mut state = State::new(255);
+        let outcome = knot_hash2(&mut state, &data);
+        println!("v2: {}", outcome);
+    }
     Ok(())
 }
 
@@ -83,5 +124,22 @@ mod tests {
         let mut state = State::new(4);
         let outcome = knot_hash(&mut state, lens);
         assert_eq!(outcome, 12);
+    }
+
+    #[test]
+    fn cases_v2() {
+        let mut lens = Vec::new();
+        lens.extend(&[17, 31, 73, 47, 23]);
+        let mut state = State::new(255);
+        let outcome = knot_hash2(&mut state, &lens);
+        let expect = "a2582a3a0e66e6e86e3812dcb672a272";
+        assert_eq!(&outcome, expect);
+
+        let mut lens = b"AoC 2017".to_vec();
+        lens.extend(&[17, 31, 73, 47, 23]);
+        let mut state = State::new(255);
+        let outcome = knot_hash2(&mut state, &lens);
+        let expect = "33efeb34ea91902bb2f59c9920caa6cd";
+        assert_eq!(&outcome, expect);
     }
 }
