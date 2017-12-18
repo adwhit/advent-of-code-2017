46250d8387208121c94fd936dfff0cdd9242a3cc
Alex Whitney <adwhit@fastmail.com>
2017-12-18 17:53:37 +0000

    day14 v2

--- src/bin/14_disk_defrag.rs
+++ src/bin/14_disk_defrag.rs
@@ -4,6 +4,9 @@ extern crate advent_of_code;
 extern crate failure;
 
 use advent_of_code::Result;
+use std::collections::{HashMap};
+
+type Array = [[bool; 128]; 128];
 
 #[derive(Clone, Debug)]
 struct State {
@@ -57,19 +60,71 @@ fn knot_hash(mut key: Vec<u8>) -> [u8; 16] {
 }
 
 fn defrag(key: &[u8]) -> u32 {
+    let array = used_array(key);
     let mut bittot = 0;
     for rowix in 0..128 {
-        let mut key = key.to_vec();
-        key.extend_from_slice((&format!("-{}", rowix).as_bytes()));
-        let hash = knot_hash(key);
-        for byte in &hash {
-            for bit in 0..8 {
-                let bitv = ((byte >> (7 - bit)) & 1u8) as u32;
-                bittot += bitv;
+        for colix in 0..128 {
+            if array[rowix][colix] {
+                bittot += 1
             }
         }
     }
     bittot
+}
+
+fn used_array(key: &[u8]) -> Array {
+    let mut array = [[false; 128]; 128];
+    for rowix in 0..128 {
+        let mut inner = [false; 128];
+        let mut key = key.to_vec();
+        key.extend_from_slice((&format!("-{}", rowix).as_bytes()));
+        let hash = knot_hash(key);
+        for (ix, byte) in hash.iter().enumerate() {
+            for bit in 0..8u8 {
+                let in_use = if ((byte >> (7 - bit)) & 1u8) == 1 { true } else { false };
+                inner[ix * 8 + bit as usize] = in_use
+            }
+        }
+        array[rowix] = inner
+    }
+    array
+}
+
+
+fn connected_components(array: &Array) -> u32 {
+    let mut grpct = 0;
+    let mut seen = HashMap::new();
+    for rowix in 0..128 {
+        for colix in 0..128 {
+            if array[rowix][colix] {
+                if seen.get(&(rowix, colix)).is_none() {
+                    // We have found a new group, do a depth-first search
+                    seen.insert((rowix, colix), grpct);
+                    dfs(rowix, colix, array, &mut seen, grpct);
+                    grpct += 1;
+                }
+            }
+        }
+    }
+    grpct
+}
+
+fn dfs(rowix: usize, colix: usize, array: &Array, seen: &mut HashMap<(usize, usize), u32>, grpct: u32) {
+    for &(r, c) in &[(-1, 0), (1, 0), (0, -1), (0, 1)] {
+        let rix = (rowix as isize + r) as usize;
+        let cix = (colix as isize + c) as usize;
+        if *array.get(rix).and_then(|arr| arr.get(cix)).unwrap_or(&false) {
+            if seen.get(&(rix, cix)).is_none() {
+                seen.insert((rix, cix), grpct);
+                dfs(rix, cix, array, seen, grpct)
+            }
+        }
+    }
+}
+
+fn defrag2(key: &[u8]) -> u32 {
+    let array = used_array(key);
+    connected_components(&array)
 }
 
 fn run() -> Result<()> {
@@ -77,8 +132,8 @@ fn run() -> Result<()> {
     let outcome = defrag(data);
     println!("v1: {}", outcome);
 
-    // let outcome = plumber2(&data)?;
-    // println!("v2: {}", outcome);
+    let outcome = defrag2(data);
+    println!("v2: {}", outcome);
 
     Ok(())
 }
@@ -104,5 +159,9 @@ mod tests {
     }
 
     #[test]
-    fn cases_v2() {}
+    fn cases_v2() {
+        let data = b"flqrgnkx";
+        let outcome = defrag2(data);
+        assert_eq!(outcome, 1242);
+    }
 }
