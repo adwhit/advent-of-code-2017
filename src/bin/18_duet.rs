3103438d134aa5711f9b6c8d947361cc05507a03
Alex Whitney <adwhit@fastmail.com>
2017-12-19 14:53:41 +0000

    day18 v1

--- src/bin/18_duet.rs
+++ src/bin/18_duet.rs
@@ -1,3 +1,5 @@
+#![feature(entry_or_default)]
+
 extern crate advent_of_code;
 #[macro_use]
 extern crate failure;
@@ -22,39 +24,46 @@ fn get_data(path: &str) -> Result<Vec<Instruction>> {
         .collect()
 }
 
-named!(parse<&str, Instruction>, switch!(take!(3),
-    "snd" => map!(anychar, |c| Instruction::Snd(c)) |
-    "set" => map!(pair!(anychar, input), |(c, inp)| Instruction::Set(c, inp)) |
-    "add" => map!(pair!(anychar, input), |(c, inp)| Instruction::Add(c, inp)) |
-    "mul" => map!(pair!(anychar, input), |(c, inp)| Instruction::Mul(c, inp)) |
-    "mod" => map!(pair!(anychar, input), |(c, inp)| Instruction::Mod(c, inp)) |
-    "rcv" => map!(anychar, |c| Instruction::Rcv(c)) |
-    "jgz" => map!(pair!(anychar, input), |(c, inp)| Instruction::Jgz(c, inp))
-));
+named!(parse<&str, Instruction>, ws!(switch!(take!(3),
+    "snd" => map!(ws!(input), |inp| Instruction::Snd(inp)) |
+    "set" => map!(pair!(ws!(anychar), input), |(c, inp)| Instruction::Set(c, inp)) |
+    "add" => map!(pair!(ws!(anychar), input), |(c, inp)| Instruction::Add(c, inp)) |
+    "mul" => map!(pair!(ws!(anychar), input), |(c, inp)| Instruction::Mul(c, inp)) |
+    "mod" => map!(pair!(ws!(anychar), input), |(c, inp)| Instruction::Mod(c, inp)) |
+    "rcv" => map!(ws!(input), |inp| Instruction::Rcv(inp)) |
+    "jgz" => map!(pair!(ws!(anychar), input), |(c, inp)| Instruction::Jgz(c, inp))
+)));
 
 named!(input<&str, Input>, alt!(
-    digit => { |ds: &str| Input::Val(ds.parse::<u32>().unwrap()) } |
+    pair!(opt!(tag!("-")), digit) => { |(n, ds): (Option<&str>, &str)| {
+        let neg = n.map_or(1, |_| -1);
+        Input::Val(neg * ds.parse::<i64>().unwrap())
+    }} |
     anychar => { |c| Input::Ptr(c) } ));
 
+#[derive(Debug, Clone, Copy)]
 enum Input {
     Ptr(char),
-    Val(u32)
+    Val(i64)
 }
 
+#[derive(Debug, Clone, Copy)]
 enum Instruction {
-    Snd(char),
+    Snd(Input),
     Set(char, Input),
     Add(char, Input),
     Mul(char, Input),
     Mod(char, Input),
-    Rcv(char),
+    Rcv(Input),
     Jgz(char, Input)
 }
 
+#[derive(Debug, Clone)]
 struct State {
-    registers: HashMap<u8, u32>,
+    registers: HashMap<char, i64>,
     code: Vec<Instruction>,
-    codeptr: usize
+    codeptr: i64,
+    last_freq: i64
 }
 
 impl State {
@@ -62,14 +71,76 @@ impl State {
         State {
             registers: HashMap::new(),
             code,
-            codeptr: 0
+            codeptr: 0,
+            last_freq: 0
+        }
+    }
+
+    fn run(&mut self) -> i64 {
+        use Instruction::*;
+        loop {
+            let inst = self.code[self.codeptr as usize];
+            match inst {
+                Set(c, inp) => {
+                    let v = self.input2val(inp);
+                    let r = self.registers.entry(c).or_insert(0);
+                    *r = v
+                },
+                Add(c, inp) => {
+                    let v = self.input2val(inp);
+                    let r = self.registers.entry(c).or_insert(0);
+                    *r += v
+                },
+                Mul(c, inp) => {
+                    let v = self.input2val(inp);
+                    let r = self.registers.entry(c).or_insert(0);
+                    *r *= v
+                },
+                Mod(c, inp) => {
+                    let v = self.input2val(inp);
+                    let r = self.registers.entry(c).or_insert(0);
+                    *r %= v
+                },
+                Jgz(c, inp) => {
+                    let r = *self.registers.entry(c).or_insert(0);
+                    if r > 0 {
+                        let v = self.input2val(inp);
+                        self.codeptr += v;
+                        continue
+                    }
+                },
+                Snd(inp) => {
+                    let v = self.input2val(inp);
+                    self.last_freq = v;
+                },
+                Rcv(inp) => {
+                    let v = self.input2val(inp);
+                    if v > 0 {
+                        return self.last_freq
+                    }
+                }
+            }
+            self.codeptr += 1;
+            if self.codeptr >= self.code.len() as i64 {
+                panic!()
+            }
+        }
+    }
+
+    fn input2val(&mut self, inp: Input) -> i64 {
+        match inp {
+            Input::Ptr(reg) => {
+                let v = self.registers.entry(reg).or_default();
+                *v
+            }
+            Input::Val(v) => v
         }
     }
 }
 
-fn duet(code: Vec<Instruction>) -> usize {
-    let state = State::new(code);
-    10
+fn duet(code: Vec<Instruction>) -> i64 {
+    let mut state = State::new(code);
+    state.run()
 }
 
 fn run() -> Result<()> {
@@ -102,9 +173,12 @@ mod tests {
 
     #[test]
     fn cases_v1() {
+        let data = get_data("data/18_test.txt").unwrap();
+        let outcome = duet(data);
+        assert_eq!(outcome, 4)
     }
 
-    #[test]
-    fn cases_v2() {
-    }
+    // #[test]
+    // fn cases_v2() {
+    // }
 }
