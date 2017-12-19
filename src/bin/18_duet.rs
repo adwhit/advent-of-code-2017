ec87eabbda3dc6a7390525a704e1f6922f5194d9
Alex Whitney <adwhit@fastmail.com>
2017-12-19 19:43:28 +0000

    day18 v2

--- src/bin/18_duet.rs
+++ src/bin/18_duet.rs
@@ -11,6 +11,7 @@ use advent_of_code::Result;
 use std::fs;
 use std::io::prelude::*;
 use std::collections::HashMap;
+use std::sync::mpsc;
 
 use nom::{anychar, digit};
 
@@ -18,9 +19,12 @@ fn get_data(path: &str) -> Result<Vec<Instruction>> {
     let mut f = fs::File::open(path)?;
     let mut s = String::new();
     f.read_to_string(&mut s)?;
-    s.lines().map(|line| parse(line)
-                  .to_result()
-                  .map_err(|_| format_err!("Parse err: {}", line)))
+    s.lines()
+        .map(|line| {
+            parse(line)
+                .to_result()
+                .map_err(|_| format_err!("Parse err: {}", line))
+        })
         .collect()
 }
 
@@ -31,7 +35,7 @@ named!(parse<&str, Instruction>, ws!(switch!(take!(3),
     "mul" => map!(pair!(ws!(anychar), input), |(c, inp)| Instruction::Mul(c, inp)) |
     "mod" => map!(pair!(ws!(anychar), input), |(c, inp)| Instruction::Mod(c, inp)) |
     "rcv" => map!(ws!(input), |inp| Instruction::Rcv(inp)) |
-    "jgz" => map!(pair!(ws!(anychar), input), |(c, inp)| Instruction::Jgz(c, inp))
+    "jgz" => map!(pair!(ws!(input), input), |(inp1, inp2)| Instruction::Jgz(inp1, inp2))
 )));
 
 named!(input<&str, Input>, alt!(
@@ -44,7 +48,7 @@ named!(input<&str, Input>, alt!(
 #[derive(Debug, Clone, Copy)]
 enum Input {
     Ptr(char),
-    Val(i64)
+    Val(i64),
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -55,24 +59,53 @@ enum Instruction {
     Mul(char, Input),
     Mod(char, Input),
     Rcv(Input),
-    Jgz(char, Input)
+    Jgz(Input, Input),
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 struct State {
+    pid: i64,
     registers: HashMap<char, i64>,
     code: Vec<Instruction>,
     codeptr: i64,
-    last_freq: i64
+    last_freq: i64,
+    tx: mpsc::Sender<i64>,
+    rx: mpsc::Receiver<i64>,
+    sendct: i64,
 }
 
 impl State {
     fn new(code: Vec<Instruction>) -> State {
+        let (tx, rx) = mpsc::channel();
         State {
+            pid: 0,
             registers: HashMap::new(),
             code,
             codeptr: 0,
-            last_freq: 0
+            last_freq: 0,
+            tx,
+            rx,
+            sendct: 0,
+        }
+    }
+
+    fn new2(
+        code: Vec<Instruction>,
+        pid: i64,
+        tx: mpsc::Sender<i64>,
+        rx: mpsc::Receiver<i64>,
+    ) -> State {
+        let mut registers = HashMap::new();
+        registers.insert('p', pid);
+        State {
+            pid,
+            registers,
+            code,
+            codeptr: 0,
+            last_freq: 0,
+            tx,
+            rx,
+            sendct: 0,
         }
     }
 
@@ -85,38 +118,38 @@ impl State {
                     let v = self.input2val(inp);
                     let r = self.registers.entry(c).or_insert(0);
                     *r = v
-                },
+                }
                 Add(c, inp) => {
                     let v = self.input2val(inp);
                     let r = self.registers.entry(c).or_insert(0);
                     *r += v
-                },
+                }
                 Mul(c, inp) => {
                     let v = self.input2val(inp);
                     let r = self.registers.entry(c).or_insert(0);
                     *r *= v
-                },
+                }
                 Mod(c, inp) => {
                     let v = self.input2val(inp);
                     let r = self.registers.entry(c).or_insert(0);
                     *r %= v
-                },
-                Jgz(c, inp) => {
-                    let r = *self.registers.entry(c).or_insert(0);
-                    if r > 0 {
-                        let v = self.input2val(inp);
-                        self.codeptr += v;
-                        continue
+                }
+                Jgz(inp1, inp2) => {
+                    let v1 = self.input2val(inp1);
+                    if v1 > 0 {
+                        let v2 = self.input2val(inp2);
+                        self.codeptr += v2;
+                        continue;
                     }
-                },
+                }
                 Snd(inp) => {
                     let v = self.input2val(inp);
                     self.last_freq = v;
-                },
+                }
                 Rcv(inp) => {
                     let v = self.input2val(inp);
                     if v > 0 {
-                        return self.last_freq
+                        return self.last_freq;
                     }
                 }
             }
@@ -127,13 +160,79 @@ impl State {
         }
     }
 
+    fn run2(&mut self) -> i64 {
+        use Instruction::*;
+        loop {
+            let inst = self.code[self.codeptr as usize];
+            match inst {
+                Set(c, inp) => {
+                    let v = self.input2val(inp);
+                    let r = self.registers.entry(c).or_insert(0);
+                    *r = v
+                }
+                Add(c, inp) => {
+                    let v = self.input2val(inp);
+                    let r = self.registers.entry(c).or_insert(0);
+                    *r += v
+                }
+                Mul(c, inp) => {
+                    let v = self.input2val(inp);
+                    let r = self.registers.entry(c).or_insert(0);
+                    *r *= v
+                }
+                Mod(c, inp) => {
+                    let v = self.input2val(inp);
+                    let r = self.registers.entry(c).or_insert(0);
+                    *r %= v
+                }
+                Jgz(inp1, inp2) => {
+                    let v1 = self.input2val(inp1);
+                    if v1 > 0 {
+                        let v2 = self.input2val(inp2);
+                        self.codeptr += v2;
+                        continue;
+                    }
+                }
+                Snd(inp) => {
+                    let v = self.input2val(inp);
+                    self.tx.send(v).unwrap();
+                    self.sendct += 1;
+                }
+                Rcv(inp) => {
+                    if let Input::Ptr(c) = inp {
+                        let r = self.registers.entry(c).or_insert(0);
+                        if self.pid == 0 {
+                            match self.rx.recv() {
+                                Ok(v) => *r = v,
+                                Err(_) => return 0, // finished
+                            }
+                        } else {
+                            let duration = std::time::Duration::from_millis(10);
+                            match self.rx.recv_timeout(duration) {
+                                Ok(v) => *r = v,
+                                Err(mpsc::RecvTimeoutError::Timeout) => return self.sendct,
+                                _ => panic!("Disconnected!"),
+                            }
+                        }
+                    } else {
+                        panic!()
+                    }
+                }
+            }
+            self.codeptr += 1;
+            if self.codeptr >= self.code.len() as i64 {
+                return self.sendct
+            }
+        }
+    }
+
     fn input2val(&mut self, inp: Input) -> i64 {
         match inp {
             Input::Ptr(reg) => {
                 let v = self.registers.entry(reg).or_default();
                 *v
             }
-            Input::Val(v) => v
+            Input::Val(v) => v,
         }
     }
 }
@@ -143,6 +242,15 @@ fn duet(code: Vec<Instruction>) -> i64 {
     state.run()
 }
 
+fn duet2(code: Vec<Instruction>) -> i64 {
+    let (tx0, rx1) = mpsc::channel();
+    let (tx1, rx0) = mpsc::channel();
+    let mut state0 = State::new2(code.clone(), 0, tx0, rx0);
+    let mut state1 = State::new2(code, 1, tx1, rx1);
+    std::thread::spawn(move || state0.run2());
+    state1.run2()
+}
+
 fn run() -> Result<()> {
     {
         let data = get_data("data/18.txt")?;
@@ -150,11 +258,11 @@ fn run() -> Result<()> {
         println!("v1: {}", outcome);
     }
 
-    // {
-    //     let data = get_data2("data/18.txt")?;
-    //     let outcome = duet(&mut state, &data);
-    //     println!("v2: {}", outcome);
-    // }
+    {
+        let data = get_data("data/18.txt")?;
+        let outcome = duet2(data);
+        println!("v2: {}", outcome);
+    }
     Ok(())
 }
 
@@ -178,7 +286,14 @@ mod tests {
         assert_eq!(outcome, 4)
     }
 
-    // #[test]
-    // fn cases_v2() {
-    // }
+    #[test]
+    fn cases_v2() {
+        let data = get_data("data/18_test.txt").unwrap();
+        let outcome = duet2(data);
+        assert_eq!(outcome, 1);
+
+        let data = get_data("data/18_test2.txt").unwrap();
+        let outcome = duet2(data);
+        assert_eq!(outcome, 3);
+    }
 }
