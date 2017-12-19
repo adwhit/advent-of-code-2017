0631f778204384c3a502cce4a9d2ce444aacdd76
Alex Whitney <adwhit@fastmail.com>
2017-12-19 08:45:31 +0000

    day17 v2

--- src/bin/17_spinlock.rs
+++ src/bin/17_spinlock.rs
@@ -1,17 +1,16 @@
 #![feature(inclusive_range_syntax)]
 
 extern crate advent_of_code;
-#[macro_use]
 extern crate failure;
 
 use advent_of_code::Result;
 use std::collections::LinkedList;
 
-fn spinlock(step: usize) -> u32 {
+fn spinlock(step: usize, ct: u32) -> u32 {
     let mut list = LinkedList::new();
     list.push_back(0);
     let mut curpos = 0;
-    for val in 1..2018 {
+    for val in 1..ct + 1 {
         curpos = (curpos + step) % list.len();
         let mut rest = list.split_off(curpos + 1);
         list.push_back(val);
@@ -22,10 +21,29 @@ fn spinlock(step: usize) -> u32 {
     *list.iter().nth(respos).unwrap()
 }
 
+fn spinlock2(step: u32, ct: u32) -> u32 {
+    let mut llen = 1;
+    let mut curpos = 0;
+    let mut zero_follow = 0;
+    for val in 1..ct {
+        curpos = (curpos + step) % llen;
+        if curpos == 0 {
+            zero_follow = val
+        }
+        curpos += 1;
+        llen += 1;
+    }
+    zero_follow
+}
+
 fn run() -> Result<()> {
     {
-        let outcome = spinlock(316);
+        let outcome = spinlock(316, 2017);
         println!("v1: {}", outcome);
+    }
+    {
+        let outcome = spinlock2(316, 50_000_000);
+        println!("v2: {}", outcome);
     }
 
     Ok(())
@@ -46,10 +64,13 @@ mod tests {
 
     #[test]
     fn cases_v1() {
-        let next = spinlock(3);
+        let next = spinlock(3, 2017);
         assert_eq!(next, 638);
     }
 
     #[test]
-    fn cases_v2() {}
+    fn cases_v2() {
+        let next = spinlock2(3, 2017);
+        assert_eq!(next, 1226);
+    }
 }
